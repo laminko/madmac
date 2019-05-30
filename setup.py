73b8dff1ac40c2aa1bae221199372ec7a0ecdd99
La Min Ko <lminko.lmk@gmail.com>
2019-05-31 00:12:15 +0630

    remove unsupported classifier 3.5.2

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5.2',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
