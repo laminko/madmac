ded5f289ab1ba64b05b495dbe79b15adeb45f071
La Min Ko <lminko.lmk@gmail.com>
2020-06-30 17:55:29 +0630

    update unittest file

--- tests/test_madmac.py
+++ tests/test_madmac.py
@@ -1,52 +1,56 @@
 from unittest import TestCase
 
+import os
+import sys
+
+module_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(module_dir)
 
 import madmac
 
 
 class TestMadMac(TestCase):
-
     def test_extract_alphanumeric_case01(self):
-        value = madmac.extract_alphanumeric('abc-def')
-        self.assertEqual(value, 'abcdef')
+        value = madmac.extract_alphanumeric("abc-def")
+        self.assertEqual(value, "abcdef")
 
     def test_extract_alphanumeric_case02(self):
-        value = madmac.extract_alphanumeric('abc:def')
-        self.assertEqual(value, 'abcdef')
+        value = madmac.extract_alphanumeric("abc:def")
+        self.assertEqual(value, "abcdef")
 
     def test_extract_alphanumeric_case03(self):
-        value = madmac.extract_alphanumeric('12:34-56')
-        self.assertEqual(value, '123456')
+        value = madmac.extract_alphanumeric("12:34-56")
+        self.assertEqual(value, "123456")
 
     def test_pair_hexvalue(self):
-        value = madmac.pair_hexvalue('abcdef', ':')
-        self.assertEqual(value, 'ab:cd:ef')
+        value = madmac.pair_hexvalue("abcdef", ":")
+        self.assertEqual(value, "ab:cd:ef")
 
     def test_int_to_hexstr(self):
         value = madmac.int_to_hexstr(255)
-        self.assertEqual(value, '0000ff')
+        self.assertEqual(value, "0000ff")
 
     def test_hexstr_to_int(self):
-        value = madmac.hexstr_to_int('0000ff')
+        value = madmac.hexstr_to_int("0000ff")
         self.assertEqual(value, 255)
 
     def test_access_object_member_case01(self):
         tmp = 1
-        value = madmac.access_object_member(tmp, 'real')
+        value = madmac.access_object_member(tmp, "real")
         self.assertEqual(value, tmp.real)
 
     def test_access_object_member_case02(self):
         tmp = []
-        value = madmac.access_object_member(tmp, 'copy')
+        value = madmac.access_object_member(tmp, "copy")
         self.assertEqual(value, tmp)
 
     def test_validate_3octets_case01(self):
-        tmp = 'aabbcc'
+        tmp = "aabbcc"
         result = madmac.validate_3octets(tmp)
         self.assertTrue(result)
 
     def test_validate_3octets_case02(self):
-        tmp = '123456'
+        tmp = "123456"
         result = madmac.validate_3octets(tmp)
         self.assertTrue(result)
 
@@ -56,16 +60,15 @@ class TestMadMac(TestCase):
         self.assertFalse(result)
 
     def test_validate_3octets_case04(self):
-        tmp = '12345'
+        tmp = "12345"
         result = madmac.validate_3octets(tmp)
         self.assertFalse(result)
 
     def test_validate_MAC_case01(self):
-        self.assertTrue(madmac.validate_MAC('12345678912'))
+        self.assertTrue(madmac.validate_MAC("12345678912"))
 
     def test_validate_MAC_case02(self):
-        self.assertTrue(madmac.validate_MAC('ab-cd-ef-12-34-56'))
+        self.assertTrue(madmac.validate_MAC("ab-cd-ef-12-34-56"))
 
     def test_validate_MAC_case03(self):
-        self.assertTrue(madmac.validate_MAC('ab:cd:ef:12:34:56'))
-
+        self.assertTrue(madmac.validate_MAC("ab:cd:ef:12:34:56"))
