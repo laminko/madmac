cf732b3efff5ff05482856072ed98ead7c5d802e
La Min Ko <lminko.lmk@gmail.com>
2020-10-22 10:59:40 +0630

    close #17

--- tests/test_madmac.py
+++ tests/test_madmac.py
@@ -97,3 +97,16 @@ class TestMadMac(TestCase):
 
     def test_validate_MAC_ValueError(self):
         self.assertFalse(madmac.validate_mac(None))
+
+    def test_mac_generator_validate(self):
+        mc = MacGenerator()
+        self.assertIsNone(mc._validate())
+
+    def test_mac_generator_build(self):
+        mc = MacGenerator()
+        mc._validate()
+        self.assertIs(str, type(list(mc._build())[-1]))
+
+    def test_mac_generator_generate(self):
+        mc = MacGenerator()
+        self.assertIs(str, type(list(mc.generate())[-1]))
